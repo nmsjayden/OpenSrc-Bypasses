9db2aa98630d1378aaf9351c35d2dc5e5ed42d6a
Ayden Serna <114900435+nmsjayden@users.noreply.github.com>
2026-06-30 18:32:10 -0500

    Update Submais.js

--- AdLinks/Console/Submais.js
+++ AdLinks/Console/Submais.js
@@ -1,1 +1,3 @@
+https://submais.paginadariqueza.com/
+
 fetch("https://app.alpharede.com/ws/submais/redirectLinks.php",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(data)}).then(r=>r.json()).then(d=>location.href=d.destination_link)
